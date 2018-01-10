be64940c2f86f361a64baa550251223b745dfbb2
KrigetsRex <35276951+KrigetsRex@users.noreply.github.com>
2018-01-10 13:44:08 -0700

    changed order

--- sizes.c
+++ sizes.c
@@ -5,13 +5,11 @@
 #include <stdio.h>
 
  int main(void) {
-   //int someVar = 0;
 
-
+   printf("size of an char is: %ld bytes\n", sizeof(char));
    printf("size of an int is: %ld bytes\n", sizeof(int));
    printf("size of an long is: %ld bytes\n", sizeof(long));
    printf("size of an double is: %ld bytes\n", sizeof(double));
-   printf("size of an char is: %ld bytes\n", sizeof(char));
    printf("size of an float is: %ld bytes\n", sizeof(float));
 
    return 0;
