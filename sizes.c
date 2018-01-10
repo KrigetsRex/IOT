a2f3113418b67fd30a8f559aa82a724b4a72ac64
KrigetsRex <35276951+KrigetsRex@users.noreply.github.com>
2018-01-10 13:43:21 -0700

    initial commit

---
+++ sizes.c
@@ -0,0 +1,18 @@
+// Luke Grice
+// CS253 in class ex
+// just to make a skeleton structure and make it
+
+#include <stdio.h>
+
+ int main(void) {
+   //int someVar = 0;
+
+
+   printf("size of an int is: %ld bytes\n", sizeof(int));
+   printf("size of an long is: %ld bytes\n", sizeof(long));
+   printf("size of an double is: %ld bytes\n", sizeof(double));
+   printf("size of an char is: %ld bytes\n", sizeof(char));
+   printf("size of an float is: %ld bytes\n", sizeof(float));
+
+   return 0;
+}
